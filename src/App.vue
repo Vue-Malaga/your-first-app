5ac10607635bb8355c4fe14d0f91abe3d928a94e
Jesus Narbona <jesusnarbona2001@gmail.com>
2022-12-27 23:13:23 +0100

    feat: added grid layout and artistLists component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,26 @@
 <script setup>
 import Header from './components/Header.vue';
 import PlaylistList from './components/PlaylistList.vue';
+import ArtistsList from './components/ArtistsList.vue';
 </script>
 
 <template>
-  <Header />
-  <PlaylistList />
-  <RouterView />
+  <div class="container">
+    <Header />
+    <PlaylistList />
+    <ArtistsList />
+    <RouterView />
+  </div>
 </template>
 
 <style scoped>
-
+.container {
+  display: grid;
+  grid-template-columns: 12vw 96vw;
+  grid-template-rows: 10vh 90vh;
+  grid-template-areas:
+    'header header'
+    'playlist artists';
+  height: 100vh;
+}
 </style>
