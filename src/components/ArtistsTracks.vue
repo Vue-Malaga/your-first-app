715a94c5d76ae9821e9e496f568690186f153a04
Jesus Narbona <jesusnarbona2001@gmail.com>
2022-12-28 23:07:50 +0100

    feat: added new componen details of singers

---
+++ src/components/ArtistsTracks.vue
@@ -0,0 +1,149 @@
+<script setup>
+import { ref } from 'vue';
+import { getById } from '../service/http.service';
+import { watch, onMounted, computed } from '@vue/runtime-core';
+import { defineProps } from '@vue/runtime-core';
+
+const artist = ref({});
+const totalTime = ref(0);
+const totalTracks = ref(0);
+const props = defineProps({
+    id: {
+        type: String,
+        required: true
+    }
+});
+
+onMounted(async () => {
+    artist.value = await getById(`http://localhost:3000/artists`, props.id);
+
+    totalTime.value = computed(() => {
+        return artist.value.tracks.reduce((acc, track) => {
+            return acc + track.duration;
+        }, 0);
+    });
+
+    totalTracks.value = computed(() => {
+        return artist.value.tracks.length;
+    });
+
+    return {
+        totalTime,
+        totalTracks
+    };
+});
+
+watch(() => props.id, async () => {
+    artist.value = await getById(`http://localhost:3000/artists`, props.id);
+});
+</script>
+
+<template>
+    <section>
+        <figure>
+            <div class="artist-image">
+                <img :src="artist.image" :alt="artist.name">
+            </div>
+            <figcaption>
+                <h3>{{ artist.name }}</h3>
+                <p><strong>{{ totalTracks }}</strong> canciones disponibles</p>
+                <p>Horas de musica <strong>{{ totalTime }}</strong> horas</p>
+            </figcaption>
+        </figure>
+        <div class="tracks">
+            <div class="track">
+                <h3><strong>Añadir</strong></h3>
+                <h3>-</h3>
+                <h3><strong>Nombre</strong></h3>
+                <h3><strong>Duración</strong></h3>
+                <h3><strong>Album</strong></h3>
+                <h3><strong>Año</strong></h3>
+            </div>
+            <div v-for="track in artist.tracks" :key="track.id" class="track">
+                <a href="">+</a>
+                <img :src="track.image" alt="track.name">
+                <p>{{ track.name }}</p>
+                <p>{{ track.duration }}</p>
+                <p>{{ track.album }}</p>
+                <p>{{ track.year }}</p>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+section {
+    overflow-y: scroll;
+    grid-area: artists;
+    padding: 1rem;
+}
+
+figure {
+    display: flex;
+    align-items: flex-end;
+    margin: 0 2rem;
+}
+
+figcaption {
+    margin: 1rem;
+}
+
+figcaption h3 {
+    font-size: 2rem;
+    font-weight: bold;
+    margin-bottom: 1rem;
+}
+
+figcaption p {
+    margin-bottom: 1rem;
+    font-size: 1rem;
+}
+
+strong {
+    font-weight: bold;
+    color: var(--accent-color-1);
+}
+
+.artist-image {
+    width: 200px;
+    height: 200px;
+    border-radius: 10px;
+    overflow: hidden;
+    border: var(--main-border)
+}
+
+.artist-image img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+
+.tracks {
+    width: 100%;
+    margin-top: 2rem;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+}
+
+.track a {
+    margin-left: 1rem;
+}
+
+.track {
+    width: 80%;
+    display: grid;
+    grid-template-columns: repeat(6, 1fr);
+    align-items: center;
+    margin-bottom: 1rem;
+}
+
+.track img {
+    width: 50px;
+    height: 50px;
+    border-radius: 10px;
+    overflow: hidden;
+    border: var(--main-border)
+}
+</style>
